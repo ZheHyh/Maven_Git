c8fe3da96901c49770a36352ff9b3458b0a55f6b
tom_glb <goodMoring_glb@qq.com>
2022-06-29 09:56:41 +0800

    test merge hot_fix

---
+++ src/main/java/com/zhe/git/HotFix.java
@@ -0,0 +1,14 @@
+package com.zhe.git;
+/*
+@Author:hyh
+@Time:
+@Version:1.0
+*/
+
+import com.sun.xml.internal.ws.api.model.wsdl.WSDLOutput;
+
+public class HotFix {
+    public static void main(String[] args) {
+        System.out.println("Hot_fix");
+    }
+}
